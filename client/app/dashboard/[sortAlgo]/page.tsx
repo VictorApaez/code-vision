56c87ce40dec838ad9e8be07df26be9397cc5942
VictorApaez <vic.apaez@gmail.com>
2023-03-22 15:59:43 -0400

    created dynamic route for displaying algos

--- client/app/dashboard/[sortAlgo]/page.tsx
+++ client/app/dashboard/[sortAlgo]/page.tsx
@@ -1,22 +1,30 @@
-import data from "../../api/data.json";
-console.log(data);
+"use client";
+import allData from "../../api/data.json";
+import CodeBlock from "./CodeBlock";
+import Description from "./Description";
 
 interface DashboardParams {
   params: { sortAlgo: string };
 }
 
-export default async function Dashboard({ params }: DashboardParams) {
+interface sortingAlgo {
+  description: string;
+}
+
+export default function Dashboard({ params }: DashboardParams) {
+  const selectedAlgo: sortingAlgo = allData.sort[params.sortAlgo];
+
   return (
     <div className="flex flex-col w-full">
       <div className="h-10 m-4">{params.sortAlgo}</div>
       <div className="flex justify-evenly h-3/6 flex-col md:flex-row">
-        <div className="bg-gray-100 p-4 rounded-md shadow-xl max-h-sm m-4 md:w-full">
-          Description
+        <div className="bg-gray-100 p-4 rounded-md shadow-xl max-h-sm m-4 md:w-full overflow-y-auto">
+          <Description desc={selectedAlgo.description} />
         </div>
-        <div className="bg-gray-100 p-4 rounded-md shadow-xl max-h-sm m-4 md:w-full">
-          Algorithm code
+        <div className="bg-gray-100 rounded-md shadow-xl max-h-sm m-4 md:w-full overflow-y-auto">
+          <CodeBlock />
         </div>
-        <div className="bg-gray-100 p-4 rounded-md shadow-xl max-h-sm m-4 md:w-full">
+        <div className="bg-gray-100 p-4 rounded-md shadow-xl max-h-sm m-4 md:w-full overflow-y-auto">
           Time &amp; space Complexity
         </div>
       </div>
