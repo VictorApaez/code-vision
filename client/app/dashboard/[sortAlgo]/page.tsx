d3fde8237fa3cd88299c0e91ed2d07208080f3c7
VictorApaez <vic.apaez@gmail.com>
2023-03-24 19:17:16 -0400

    importing time complexity and visualizer card

--- client/app/dashboard/[sortAlgo]/page.tsx
+++ client/app/dashboard/[sortAlgo]/page.tsx
@@ -1,6 +1,8 @@
 "use client";
 import allData from "../../api/data.json";
+import SortingVisualizer from "./(bar-graph)/SortingVisualizer";
 import CodeBlock from "./CodeBlock";
+import Complexity from "./Complexity";
 import Description from "./Description";
 
 interface DashboardParams {
@@ -17,20 +19,20 @@ export default function Dashboard({ params }: DashboardParams) {
   return (
     <div className="flex flex-col w-full">
       <div className="h-10 m-4">{params.sortAlgo}</div>
-      <div className="flex justify-evenly h-3/6 flex-col md:flex-row">
+      <div className="flex justify-evenly h-2/6 flex-col md:flex-row">
         <div className="bg-gray-100 p-4 rounded-md shadow-xl max-h-sm m-4 md:w-full overflow-y-auto">
           <Description desc={selectedAlgo.description} />
         </div>
         <div className="bg-gray-100 rounded-md shadow-xl max-h-sm m-4 md:w-full overflow-y-auto">
           <CodeBlock />
         </div>
-        <div className="bg-gray-100 p-4 rounded-md shadow-xl max-h-sm m-4 md:w-full overflow-y-auto">
-          Time &amp; space Complexity
+        <div className="bg-gray-100 p-4 rounded-md shadow-xl max-h-sm m-4 md:w-full overflow-y-auto flex flex-col ">
+          <Complexity />
         </div>
       </div>
 
-      <div className="bg-gray-100 p-4 rounded-md shadow-xl  h-3/6 m-4 ">
-        Visualizer
+      <div className="flex flex-col bg-gray-100 p-4 rounded-md shadow-xl  h-4/6 m-4 ">
+        <SortingVisualizer />
       </div>
     </div>
   );
