bdf4d4fa0fb1ffa47856ac7707d47bdb21226601
VictorApaez <vic.apaez@gmail.com>
2023-03-27 08:54:31 -0400

    refactored code and gave each bar 100% width to make them dynamic

--- client/app/dashboard/[sortAlgo]/(bar-graph)/Bar.tsx
+++ client/app/dashboard/[sortAlgo]/(bar-graph)/Bar.tsx
@@ -1,34 +1,33 @@
 import { motion, useAnimation } from "framer-motion";
 
-export interface Props {
+interface Props {
   item: {
     value: number;
     key: string;
   };
   isSwapping: boolean;
 }
+
 export const Bar = ({ item, isSwapping }: Props) => {
   const controls = useAnimation();
+  const height = item.value * 5 + "%";
+
   const swappingStyles = {
-    height: item.value * 5 + "%",
-    minWidth: "50px",
+    height,
     backgroundColor: "#e4c517",
   };
   const regularStyles = {
-    height: item.value * 5 + "%",
-    background: `linear-gradient(0deg, rgba(255,0,0,1) 0%, #6b0505 ${
-      item.value * 5
-    }%)`,
-    minWidth: "50px",
+    height,
+    background: `linear-gradient(0deg, rgba(255,0,0,1) 0%, #6b0505 ${height})`,
     boxShadow: "none",
   };
   return (
     <motion.div
       layout
-      id={`${item.key}`}
-      className={`p-3 cursor-move transition-colors duration-200 rounded-md`}
+      id={item.key}
+      className="cursor-move transition-colors duration-200 rounded-md w-full mx-1"
       animate={controls}
       style={isSwapping ? swappingStyles : regularStyles}
-    ></motion.div>
+    />
   );
 };
