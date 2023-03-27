02febe1c006ff35bdde8e0a3dd2ff91eede95226
VictorApaez <vic.apaez@gmail.com>
2023-03-27 13:49:05 -0400

    refactored code and removed bar graph controllers and sorting algo to utils

--- client/app/dashboard/[sortAlgo]/(bar-graph)/SortingVisualizer.tsx
+++ client/app/dashboard/[sortAlgo]/(bar-graph)/SortingVisualizer.tsx
@@ -1,7 +1,8 @@
+import { BarGraph } from "./barGraph";
 import { Controller } from "./Controller";
-import { Bar } from "./Bar";
 import React, { useState } from "react";
 import { AnimatePresence } from "framer-motion";
+import { bubbleSort } from "@/app/utils/sortingAlgos/bubbleSort";
 
 const initialItems = [
   { key: "item1", value: 20 },
@@ -20,86 +21,30 @@ export default function QuickSortVisualizer() {
   const [items, setItems] = useState(initialItems);
   const [swappingItems, setSwappingItems] = useState([-1, -1]);
   const [disableBtn, setDisableBtn] = useState(false);
+  const [animationSpeed, setAnimationSpeed] = useState(120);
 
   const animateSorting = async () => {
     setDisableBtn(true);
-    let newItems = [...items];
-    let sortedItems = false;
-
-    while (!sortedItems) {
-      sortedItems = true;
-      for (let i = 0; i < newItems.length - 1; i++) {
-        setSwappingItems([i, i + 1]);
-        await new Promise((resolve) => setTimeout(resolve, 500));
-        if (newItems[i].value > newItems[i + 1].value) {
-          sortedItems = false;
-          const temp = newItems[i];
-          newItems[i] = newItems[i + 1];
-          newItems[i + 1] = temp;
-
-          setItems([...newItems]);
-          await new Promise((resolve) => setTimeout(resolve, 500));
-        }
-      }
-    }
-    setSwappingItems([-1, -1]);
+    await bubbleSort({ items, setItems, animationSpeed, setSwappingItems });
     setDisableBtn(false);
   };
-
-  function addBar() {
-    let array = [...items];
-    let randomNum = Math.floor(Math.random() * 20);
-    const newItem = {
-      key: `item${array.length + 1}`,
-      value: randomNum,
-    };
-    array.push(newItem);
-    setItems(array);
-  }
-
-  function removeBar() {
-    let array = [...items];
-    array.pop();
-    setItems(array);
-  }
-
-  function randomizeBars() {
-    let array = [...items];
-    let currentIndex = array.length,
-      randomIndex;
-
-    while (currentIndex != 0) {
-      randomIndex = Math.floor(Math.random() * currentIndex);
-      currentIndex--;
-
-      [array[currentIndex], array[randomIndex]] = [
-        array[randomIndex],
-        array[currentIndex],
-      ];
-    }
-
-    setItems([...array]);
-  }
 
   return (
     <>
       <Controller
+        items={items}
+        setItems={setItems}
         animateSorting={animateSorting}
-        randomizeBars={randomizeBars}
         disableBtn={disableBtn}
-        addBar={addBar}
-        removeBar={removeBar}
+        length={items.length}
+        setAnimationSpeed={setAnimationSpeed}
       />
       <AnimatePresence>
-        <div className="flex flex-1 justify-evenly items-end box-border	">
-          {items.map((item, index) => (
-            <Bar
-              key={item.key}
-              item={item}
-              isSwapping={swappingItems.includes(index)}
-            />
-          ))}
-        </div>
+        <BarGraph
+          animationSpeed={animationSpeed}
+          items={items}
+          swappingItems={swappingItems}
+        />
       </AnimatePresence>
     </>
   );
