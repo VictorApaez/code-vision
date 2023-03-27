c48f7038de644f188dd4044f66831c1285c29af9
VictorApaez <vic.apaez@gmail.com>
2023-03-27 11:59:53 -0400

    changed the swap animation speed to be the same as the sorting speed

--- client/app/dashboard/[sortAlgo]/(bar-graph)/Bar.tsx
+++ client/app/dashboard/[sortAlgo]/(bar-graph)/Bar.tsx
@@ -1,4 +1,4 @@
-import { motion, useAnimation } from "framer-motion";
+import { motion } from "framer-motion";
 
 interface Props {
   item: {
@@ -6,10 +6,10 @@ interface Props {
     key: string;
   };
   isSwapping: boolean;
+  animationSpeed: number;
 }
 
-export const Bar = ({ item, isSwapping }: Props) => {
-  const controls = useAnimation();
+export const Bar = ({ item, isSwapping, animationSpeed }: Props) => {
   const height = item.value * 5 + "%";
 
   const swappingStyles = {
@@ -21,13 +21,14 @@ export const Bar = ({ item, isSwapping }: Props) => {
     background: `linear-gradient(0deg, rgba(255,0,0,1) 0%, #6b0505 ${height})`,
     boxShadow: "none",
   };
+
   return (
     <motion.div
       layout
       id={item.key}
       className="cursor-move transition-colors duration-200 rounded-md w-full mx-1"
-      animate={controls}
       style={isSwapping ? swappingStyles : regularStyles}
+      transition={{ duration: animationSpeed / 1000 }}
     />
   );
 };
