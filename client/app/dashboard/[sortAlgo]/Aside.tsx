5b60cbca6811d8e00a18141c0cfc38f8271ad78b
VictorApaez <vic.apaez@gmail.com>
2023-03-28 16:17:35 -0400

    updated logo

--- client/app/dashboard/[sortAlgo]/Aside.tsx
+++ client/app/dashboard/[sortAlgo]/Aside.tsx
@@ -4,7 +4,7 @@ export default function Aside() {
   return (
     <div className="p-5 bg-[#0A1532] shadow-[5px_5px_20px_rgb(0,0,0,0.4)] text-gray-100">
       <h1 className="text-3xl font-normal leading-normal mt-0 mb-2  mb-4">
-        CodeVisualizer
+        CodeVision
       </h1>
       <ul className="flex flex-col justify-center items-center pt-3 cursor-pointer">
         <li>Sorting Algorithms</li>
