229dab228bbc515a9cfd606774d78f6709778fc5
VictorApaez <vic.apaez@gmail.com>
2023-03-28 10:20:19 -0400

    removed hard coded sort algo and placed in seperate file

--- client/app/dashboard/[sortAlgo]/CodeBlock.tsx
+++ client/app/dashboard/[sortAlgo]/CodeBlock.tsx
@@ -1,32 +1,6 @@
 import { CopyBlock, dracula } from "react-code-blocks";
 
-export default function CodeBlock() {
-  const code = `
-function quicksort(arr: number[]): number[] {
-  if (arr.length <= 1) {
-    return arr;
-  }
-
-  const pivotIndex = Math.floor(arr.length / 2);
-  const pivot = arr[pivotIndex];
-  const less = [];
-  const greater = [];
-
-  for (let i = 0; i < arr.length; i++) {
-    if (i === pivotIndex) {
-      continue;
-    }
-
-    if (arr[i] < pivot) {
-      less.push(arr[i]);
-    } else {
-      greater.push(arr[i]);
-    }
-  }
-
-  return [...quicksort(less), pivot, ...quicksort(greater)];
-}
-`;
+export default function CodeBlock({ code }: { code: string }) {
   return (
     <CopyBlock
       text={code}
